0846bce6547ab91843107110b00ef7cce8a7d9d4
Nhat <nhatanh1nguyen@gmail.com>
2020-12-16 21:26:44 +0100

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
 import { FormsModule } from '@angular/forms';
-import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent,
-  ],
+  declarations: [AppComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
