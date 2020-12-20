3030c90d51bef57ed222ebc4262b5f60e7d54ff1
Nhat <nhatanh1nguyen@gmail.com>
2020-12-20 21:33:29 +0100

    sub

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -10,13 +10,13 @@ import { ShoppingListService } from './shopping-list.service';
 })
 export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[];
-  private igChangeSub: Subscription;
+  private subscription: Subscription;
 
   constructor(private slService: ShoppingListService) {}
 
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
-    this.igChangeSub = this.slService.ingredientsChanged.subscribe(
+    this.subscription = this.slService.ingredientsChanged.subscribe(
       (ingredients: Ingredient[]) => {
         this.ingredients = ingredients;
       }
@@ -24,6 +24,6 @@ export class ShoppingListComponent implements OnInit {
   }
 
   ngOnDestroy() {
-    this.igChangeSub.unsubscribe();
+    this.subscription.unsubscribe();
   }
 }
